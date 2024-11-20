cd1678bf4eb9209e709c9ad468aed0438acb74cb
suyanthapa <146444078+suyanthapa@users.noreply.github.com>
2024-11-20 10:49:14 +0545

    continue with email/ username

--- code/header.php
+++ code/header.php
@@ -23,32 +23,4 @@
     </form>
   </div>
 </div>
-
-<!-- Signup Modal -->
-<div id="signupModal" class="modal modal-overlay" style="display: none;">
-  <div class="modal-content">
-    <div class="modal-left">
-      <h2>Success starts here</h2>
-      <ul>
-        <li>Over 700 categories</li>
-        <li>Quality work done faster</li>
-        <li>Access to talent and businesses across the globe</li>
-      </ul>
-      
-      <img src="login-image.png" alt="" class="login-image">
-    </div>
-
-    <div class="modal-right">
-      <h2>Sign in to your account</h2>
-      <p>Don't have an account? <a href="#">Join here</a></p>
-      <button class="social-login google">Continue with Google</button>
-      <button class="social-login email">Continue with email/username</button>
-      <div class="or">OR</div>
-      <button class="social-login apple">Apple</button>
-      <button class="social-login facebook">Facebook</button>
-      <p class="terms">
-        By joining, you agree to the Terms of Service and occasionally receive emails from us.
-      </p>
-    </div>
-  </div>
-</div>
+<?php include('signup.php'); ?>
