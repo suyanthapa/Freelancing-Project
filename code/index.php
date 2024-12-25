82a7b5b1adc313f84da0a75b2fd011c211e785cd
Anish Baniya <anishbaniya07@gmail.com>
2024-12-25 10:09:06 +0545

    feat: dashboard updated

--- code/index.php
+++ code/index.php
@@ -1,15 +1,15 @@
-
 <?php
 
-      session_start();
+session_start();
 
-      // include("connection.php");
-      // include("function.php");
+// include("connection.php");
+// include("function.php");
 
-      // $user_data= check_login($con);
+// $user_data= check_login($con);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,16 +17,21 @@
   <link rel="stylesheet" href="css/styles.css">
 
   <style>
-   
+
   </style>
 </head>
+
 <body>
   <!-- Header-Section -->
- <?php include('header.php');?>
-   
+  <?php include('header.php'); ?>
+
   <!-- Dashboaard-Section -->
   <?php include('dashboard.php'); ?>
 
+  <!-- Footer Section -->
+  <?php include('footer.php') ?>
+
   <script src="js/script.js"></script>
 </body>
+
 </html>
